75cb98d8b3140ee6f64faa35c2e0d81cd1873874
Fleaux <hudsonfloyd@yahoo.com>
2023-01-19 21:02:40 -0600

    created the function to populate the API data into the modal

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -7,3 +7,25 @@ function horoscopeDetials(zodiacSign){
     .then(response => response.json())
     .then(showHoroscopeDetails); 
 }
+
+// function to render the API data into the modal 
+function showHoroscopeDetails(response,zodaicSign){
+    let current_date = document.getElementById('current_date');
+    let today = new Date().toString().split(' ').splice(1,3).join(' ');
+    current_date.innerText = today;
+
+    let lucky_time = document.getElementById('lucky_time');
+    lucky_time.innerText= `${response.lucky_time}`;
+
+    let lucky_number = document.getElementById('lucky_number');
+    lucky_number.innerText = `${response.lucky_number}`;
+    
+    let color = document.getElementById('color');
+    color.innerText = `${response.color}`;
+
+    let mood = document.getElementById('mood');
+    mood.innerText = `${response.mood}`;
+
+    let description = document.getElementById('description');
+    description.innerText = `${response.description}`;
+}
